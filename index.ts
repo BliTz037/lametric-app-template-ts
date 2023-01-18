a2cc853948a7ede4a5393f46fe4e34dae9763f6b
BliTz <60670940+BliTz037@users.noreply.github.com>
2023-01-18 16:54:41 +0100

    [ADD] Controller, service, and class for frames

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import ApiController from './src/controllers/api.controller';
 
 const main = async () => {
     const app = express();
@@ -8,6 +9,8 @@ const main = async () => {
 
     app.use(express.json());
     app.use(cors());
+
+    app.use(ApiController);
 
     app.use((req, res) => {
         res.status(404).json({ message: "Route not found !" })
